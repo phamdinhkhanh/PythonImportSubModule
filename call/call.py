6db68b267c945c8996e8881336294bcca9579781
phamdinhkhanh <phamdinhkhanh.tkt53.neu@gmail.com>
2020-09-11 12:50:44 +0700

    first commit

---
+++ call/call.py
@@ -0,0 +1,10 @@
+
+import os
+import sys
+import os
+
+sys.path.append("..")
+
+from PythonSubModuleImport.lib.boxtime import foo
+
+foo()
---
+++ call/call2.py
@@ -0,0 +1,9 @@
+
+import os
+import sys
+import os
+
+sys.path.append(".")
+from lib.boxtime import foo
+
+foo()
